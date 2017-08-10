c567af7279976376288848be802228376e3b8e01
Andrey But <andrey.but@saritasa.com>
2017-08-10 11:18:00 +0600

    remove rabbit

---
+++ config/settings/common/security.py
@@ -0,0 +1,3 @@
+AMQP_CONFIG = {'AMQP_URI': "amqp://guest:guest@rabbit"}
+TOKEN_GOSHIPPO = {'LIVE_TOKEN': 'shippo_live_6a3b0e01b47f7e814bf7b74c35561703aaeb547c',
+                  'TEST_TOKEN': 'shippo_test_55dfc05531b49ed2e711fa2cca863d72c68f87b7'}
