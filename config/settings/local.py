ebcd8cefc9c84c0006e1f80db8490405ed582b06
Andre A. But <andrey.but@saritasa.com>
2017-08-24 22:00:33 +0600

    Prepare testing, resloved review comment in db, removed some kwargs

---
+++ config/settings/local.py
@@ -0,0 +1,15 @@
+"""
+Address of store, from send parcels
+
+"""
+ADDRESS_FROM = {
+    'name': 'PythonCamp',
+    'company': 'PythonCampTeam',
+    'street1': '1234 Main Street',
+    'city': 'San Francisco',
+    'state': 'CA',
+    'zip': '94111',
+    'country': 'USA',
+    'phone': '0015553419393',
+    'email': 'varvara.malysheva@saritasa.com',
+}
